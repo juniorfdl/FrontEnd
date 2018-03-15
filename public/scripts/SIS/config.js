0044284a58485bbf5f37120938991a0c0c7d5907
Elvio Junior <juniorfdl@terra.com.br>
2018-03-14 22:18:38 -0300

    backend node

---
+++ public/scripts/SIS/config.js
@@ -0,0 +1,16 @@
+var App;
+(function (App) {
+    'use strict';
+
+    App.modules.App.constant('luarApp',
+    {        
+        BASEURL: 'http://localhost:51309/',
+        WEBAPI: 'http://localhost:51309/',
+        ITENS_POR_PAGINA: '15',
+        CaminhoPDF: 'http://localhost/PDFNfse/',
+        apiDelphi: 'http://localhost:1234/api/'
+        //ISDEBUG: true
+    });
+
+})(App || (App = {}));
+//# sourceMappingURL=app.js.map
